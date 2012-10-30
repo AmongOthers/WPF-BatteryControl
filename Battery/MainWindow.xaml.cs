bda40f612cd15e0374ea420ef7c4be31a79efef3
amongothers <zhengwenwei.123@gmail.com>
2012-10-30 17:26:14 +0800

    first commmit

---
+++ Battery/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Battery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Thread thread = new Thread(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    i++;
+                    if (i > 314)
+                    {
+                        i = 0;
+                    }
+                    this.Dispatcher.Invoke(new Action(() =>
+                        {
+                            this.battery.pthFiller.Width = i;
+                        }));
+                    Thread.Sleep(100);
+                }
+            });
+            thread.Start();
+        }
+    }
+}
